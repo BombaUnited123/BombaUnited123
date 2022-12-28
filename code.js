d4032a49cb586087f7bf8ee5d99543ff0f5dd2ce
BombaUnited123 <114979109+BombaUnited123@users.noreply.github.com>
2022-12-28 15:57:44 +0700

    code update.js

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ function.querySelectorAll(); innerGame List.AddEventListener.append(); instance.
   const-static.string = {
        static.function for(i+1-6,1 i++1.1÷6"1*3$5-6%88>7++i 1++i1.9€87π92**+9-82();85#9[]9¶∆92×836°=9286>©3code92@17`82.QueryF826£¢%•™®(#;"726:6'+88++i+j+f_86+9()7411@°|•¢=¶86 = 83729183);
        let if for loop break ;
+    InstallArgs, Quick =.> do if let ; namedata"onclick(data.if{none!**;#@$&£¢€¥π÷%÷π¶∆®©™[1],)*(kotlins:#;/)";:
   }
   oninput(); function.static.queryselectorAll(); = {};
     return 2; end
