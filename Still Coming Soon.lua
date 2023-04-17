9e06e0606309b58a273ffe4822b82b9352535134
BombaUnited123 <114979109+BombaUnited123@users.noreply.github.com>
2023-04-17 15:12:53 +0700

    A Committed File For A Rawed :)

---
+++ Still Coming Soon.lua
@@ -0,0 +1,83 @@
+--Named Your Script This(Not Optional)And User--
+
+local StringByInputId = BombalibTezt.lua,
+
+local UserNameInput = true, 
+
+local Icons = rbxassetid://4483345998,
+
+local UserIcons = player.icons, 
+
+--Local Color Command--
+
+MainMenuColor(RGB('255,255,255'),
+
+LocalRgb('255,79,0'),
+
+LocalRgbPercent('100,31,0'),
+
+LocalCMYK('0,69, 00,0'),
+
+Calculated('255*65536+255*256+255'), --White--
+
+Calculated('0*65536+0*256+255'), --Blue--
+
+Calculated('255*65536+0*256+0'), --Red--
+
+Calculated('0*65536+255*256+0'), --Green--
+
+Calculated('128*65536+128*256+128'), --Grey--
+
+Calculated('255*65536+255*256+0'), --Yellow--
+
+PermanentRgb = ('#FF4F00'),
+
+PermanentExposure = ('572,726,873'),
+
+PermanentAutomaticlySpectrumtricsRGBA = ('169,236,174,210'),
+
+PermanentLocal = ('4921,8309,9382,729291096422,98382069171531,828922881,653432210008,73829918,8736288,83728187,8652619,8202,8276264211242101000265421344380677643470,83929176,827,7,73929197545633,98292927643221253,83872917,73762618,83729,83,8739272,7899,8202018642113538,98,9390201,83754355638,882882,828298,828,820109865422343,88372919644,7372798,8288,9388020086432122452804086432357759864292,83728854,6277,7635347,836685,83759,87362,83802816421,8389,982829,93863526422534,638'),
+
+local HSV = 19,100.0,100.0,
+
+local HSL = 19,100.0,50.0,
+
+--Local Function Of His Script--
+
+local strings = Value
+
+if function getgenv()
+
+   game.GetService()
+
+     if plugins => (value) {
+
+          if else {
+
+                 color.getgenv() => Workspace[](){}% 
+
+              end
+
+            }
+
+       end
+
+     }
+
+end
+
+child.parent function()
+
+    script.parent(return => filled){--DON'T FILL THIS--}<
+
+end
+
+getgenv == if game.PlaceId == %Num%<*--Place Id Most Seperated With Game--, --Add "then" For If Ends Line-- then
+
+main.print('Script Has Executed'),
+
+second.print('Script Already Been Executed'),
+
+third.print('Runnable If Available At %countDown%<*,'),
+
+-- Still Coming Soon :) --
